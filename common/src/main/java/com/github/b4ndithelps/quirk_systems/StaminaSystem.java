adaca43e5380c85b47468e679f5ef57c30a6049c
Bandit <bandit.lbh@proton.me>
2025-07-06 22:25:06 -0400

    test files

---
+++ common/src/main/java/com/github/b4ndithelps/quirk_systems/StaminaSystem.java
@@ -0,0 +1,18 @@
+package com.github.b4ndithelps.quirk_systems;
+
+/**
+ * Handles all the stamina interactions for the player. Including, but not limited to:
+ * - Max Stamina Calculation
+ * - Stamina Growth
+ * - Stamina Usage
+ * - Exhaustion System (Put exhaustion in its own?)
+ */
+public class StaminaSystem {
+
+
+
+
+
+
+
+}
