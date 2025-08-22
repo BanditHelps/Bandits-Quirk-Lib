d13f12fe0b44ef162df00b251cae88d2b986f49c
Bandit <bandit.lbh@proton.me>
2025-08-22 01:43:19 -0400

    added research table to library

---
+++ forge/src/main/java/com/github/b4ndithelps/forge/blocks/ModBlocks.java
@@ -0,0 +1,35 @@
+package com.github.b4ndithelps.forge.blocks;
+
+import com.github.b4ndithelps.BanditsQuirkLib;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, BanditsQuirkLib.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BanditsQuirkLib.MOD_ID);
+
+    public static final RegistryObject<Block> RESEARCH_TABLE_BLOCK = registerBlock("research_table_block", () -> new ResearchTableBlock(BlockBehaviour.Properties.of().mapColor(MapColor.METAL).strength(2.0F, 6.0F).sound(SoundType.METAL)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> blockSupplier) {
+        RegistryObject<T> block = BLOCKS.register(name, blockSupplier);
+        ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+        return block;
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+        ITEMS.register(eventBus);
+    }
+}
+
+
